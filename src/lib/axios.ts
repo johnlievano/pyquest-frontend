6b9d7228938c6d4d5aac0cd0a67441ffea229255
nmaster <johnlievanom@gmail.com>
2026-05-25 12:08:12 -0500

    config: usar variable de entorno en Axios para producción

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // 1. Instancia base
 const apiClient = axios.create({
-  baseURL: 'https://pyquest-backend.onrender.com/api',
+  baseURL: import.meta.env.VITE_API_URL ?? 'http://localhost:8218/api',
   headers: {
     'Content-Type': 'application/json',
   },
